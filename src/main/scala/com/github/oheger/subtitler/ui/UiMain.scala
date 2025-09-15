0c1de35a03d82c8ec1024bcda466eef10a7a80a7
Oliver Heger <oliver.heger@oliver-heger.de>
2025-09-15 11:54:03 +0200

    feat(ui): Extended the main window.
    Added a pane with configuration options for a stream to capture audio
    and generate subtitles. The UI controls are not yet connected to any
    logic.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- src/main/scala/com/github/oheger/subtitler/ui/UiMain.scala
+++ src/main/scala/com/github/oheger/subtitler/ui/UiMain.scala
@@ -16,11 +16,20 @@
 
 package com.github.oheger.subtitler.ui
 
+import javafx.event.EventHandler
 import scalafx.application.JFXApp3
+import scalafx.geometry.Insets
+import scalafx.geometry.Pos.Center
 import scalafx.scene.Scene
-import scalafx.scene.layout.VBox
-import scalafx.scene.text.Text
+import scalafx.scene.control.{Button, ComboBox, Label, TextField, TitledPane}
+import scalafx.scene.layout.{BorderPane, HBox, VBox}
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Circle
 
+/**
+  * The main class of the ''subtitler'' application. It creates the main
+  * window.
+  */
 object UiMain extends JFXApp3:
   override def start(): Unit =
     stage = new JFXApp3.PrimaryStage:
@@ -28,8 +37,33 @@ object UiMain extends JFXApp3:
       width = 640
       height = 480
       scene = new Scene:
-        content = new VBox:
-          children = Seq(
-            new Text:
-              text = "<subtitle>"
+        content = new BorderPane:
+          padding = Insets(20, 20, 20, 20)
+          center = new TitledPane:
+            text = "Configuration"
+            collapsible = false
+            content = new VBox:
+              padding = Insets(20, 20, 20, 20)
+              spacing = 5
+              children = Seq(
+                new Label:
+                  text = "Model path:",
+                new HBox:
+                  spacing = 20
+                  alignment = Center
+                  children = Seq(
+                    new TextField:
+                      prefWidth = 200,
+                    new Button:
+                      text = "Select..."
+                      onAction.value = _ => println("Button 'Select' clicked.")
+                  ),
+                new Label:
+                  text = "Input device:",
+                new ComboBox[String](items = Seq("Mixer1", "Mixer2", "Mixer3")),
+                new Button:
+                  margin = Insets(top = 20, right = 0, bottom = 0, left = 0)
+                  text = "Start subtitles"
+                  padding = Insets(20, 20, 20, 20)
+                  graphic = Circle(radius = 10, fill = Color(red = 0.8, green = 0, blue = 0, opacity = 1))
           )
