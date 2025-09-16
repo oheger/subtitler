23a4e66ee5bc5321477ea98031737ea1f53941ce
Oliver Heger <oliver.heger@oliver-heger.de>
2025-09-16 22:07:13 +0200

    feat(ui): Connected config UI to controller.
    The configuration UI now obtains the values for the input controls
    from a `Controller` instance. The code has been extracted into a
    separate function. Preparations have been made to have multiple views.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- src/main/scala/com/github/oheger/subtitler/ui/UiMain.scala
+++ src/main/scala/com/github/oheger/subtitler/ui/UiMain.scala
@@ -22,16 +22,23 @@ import scalafx.geometry.Insets
 import scalafx.geometry.Pos.Center
 import scalafx.scene.Scene
 import scalafx.scene.control.{Button, ComboBox, Label, TextField, TitledPane}
-import scalafx.scene.layout.{BorderPane, HBox, VBox}
+import scalafx.scene.layout.{BorderPane, HBox, StackPane, VBox}
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.Circle
+
+import scala.compiletime.uninitialized
 
 /**
   * The main class of the ''subtitler'' application. It creates the main
   * window.
   */
 object UiMain extends JFXApp3:
+  /** The object controlling the application state. */
+  private var controller: Controller = uninitialized
+
   override def start(): Unit =
+    controller = new Controller
+
     stage = new JFXApp3.PrimaryStage:
       title = "Subtitler"
       width = 640
@@ -39,31 +46,61 @@ object UiMain extends JFXApp3:
       scene = new Scene:
         content = new BorderPane:
           padding = Insets(20, 20, 20, 20)
-          center = new TitledPane:
-            text = "Configuration"
-            collapsible = false
-            content = new VBox:
-              padding = Insets(20, 20, 20, 20)
-              spacing = 5
-              children = Seq(
-                new Label:
-                  text = "Model path:",
-                new HBox:
-                  spacing = 20
-                  alignment = Center
-                  children = Seq(
-                    new TextField:
-                      prefWidth = 200,
-                    new Button:
-                      text = "Select..."
-                      onAction.value = _ => println("Button 'Select' clicked.")
-                  ),
-                new Label:
-                  text = "Input device:",
-                new ComboBox[String](items = Seq("Mixer1", "Mixer2", "Mixer3")),
-                new Button:
-                  margin = Insets(top = 20, right = 0, bottom = 0, left = 0)
-                  text = "Start subtitles"
-                  padding = Insets(20, 20, 20, 20)
-                  graphic = Circle(radius = 10, fill = Color(red = 0.8, green = 0, blue = 0, opacity = 1))
-          )
+          center = new StackPane:
+            children = Seq(
+              configPane
+            )
+
+    controller.setUp()
+
+  override def stopApp(): Unit =
+    controller.shutdown()
+    super.stopApp()
+
+  /**
+    * Returns the pane for the configuration settings.
+    *
+    * @return the configuration pane
+    */
+  private def configPane: TitledPane =
+    new TitledPane:
+      text = "Configuration"
+      collapsible = false
+      content = new VBox:
+        padding = Insets(20, 20, 20, 20)
+        spacing = 5
+        children = Seq(
+          new Label:
+            text = "Model path:",
+          new HBox:
+            spacing = 20
+            alignment = Center
+            children = Seq(
+              new TextField:
+                prefWidth = 200
+                text <==> controller.modelPath,
+              new Button:
+                text = "Select..."
+                onAction.value = _ => controller.chooseModelPath(stage)
+            ),
+          new Label:
+            text = "Input device:",
+          new HBox:
+            spacing = 20
+            alignment = Center
+            children = Seq(
+              new ComboBox[String]:
+                items <==> controller.inputDevices
+                value <==> controller.selectedInputDevice,
+              new Button:
+                text = "Reload"
+                onAction.value = _ => controller.updateInputDevices()
+            ),
+          new Button:
+            margin = Insets(top = 20, right = 0, bottom = 0, left = 0)
+            text = "Start subtitles"
+            padding = Insets(20, 20, 20, 20)
+            graphic = Circle(radius = 10, fill = Color(red = 0.8, green = 0, blue = 0, opacity = 1))
+            disable <== !controller.canStartRecognizerStream
+        )
+
